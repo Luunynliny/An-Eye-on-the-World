ab479d3f651dcfae5aa1fc33b83802eee17c1255
Luunynliny <thomasmattone.perso@gmail.com>
2024-04-22 00:08:01 +0200

    add articles quotations retrieval

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,13 @@
-import bs4.element as bs4e
+from os.path import join, dirname
+
 import requests
 from bs4 import BeautifulSoup
+from dotenv import dotenv_values
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+from selenium.webdriver.common.by import By
+
+ENV: dict[str, str] = dotenv_values(join(dirname(__file__), ".env"))
 
 
 def get_soup(url: str) -> BeautifulSoup:
@@ -8,8 +15,55 @@ def get_soup(url: str) -> BeautifulSoup:
     return BeautifulSoup(response.text, 'html.parser')
 
 
-def get_non_abrogated_articles(soup: BeautifulSoup) -> bs4e.ResultSet:
-    return soup.select(".articleLink:not(.abrogated)")
+def get_code_non_abrogated_articles(code_id: str) -> list[str]:
+    """
+    Args:
+        code_id (str): id of a Code.
+
+    Returns:
+        list[str]: ids of non-abrogated articles within the Code.
+    """
+    soup = get_soup(join(ENV["CODES_DB_URL"], code_id, ENV["DATE"]))
+    return [element.get("id")[3:] for element in soup.select(".articleLink:not(.abrogated)")]
 
 
-soup = get_soup("https://example.com")
+def get_article_data(article_id: str) -> tuple[str, list[str]]:
+    """
+    Args:
+        article_id (): id of an Article.
+
+    Returns:
+        tuple[str, list[str]]: name of the Article and Codes' Articles' id quoting the Article.
+    """
+    ###
+    # An Article page could contain a button to display all the other Codes' Articles where the Article is quoted
+    # If so, we need to click on this button to make these informations appear on the page
+    options = webdriver.FirefoxOptions()
+    options.add_argument("-headless")
+
+    driver = webdriver.Firefox(options=options)
+    driver.get(join(ENV["ARTICLES_DB_URL"], article_id, ENV["DATE"]))
+
+    is_article_quoted = True
+    try:
+        driver.find_element(By.ID, f"tip-tab-liens-{article_id}-1-button").click()
+    except NoSuchElementException:
+        is_article_quoted = False
+
+    soup = BeautifulSoup(driver.page_source, 'html.parser')
+    ###
+
+    name = soup.select(".name-article span")[0].text
+    relative_codes_article_ids = []
+
+    if is_article_quoted:
+        # Only keep quotations from other Code's Articles
+        for link in soup.select(".relative-link li a"):
+            _id = link.get("href").split("#")[1]
+            text = link.text
+
+            if text.split()[0] in ["Code", "Livre"]:  # "Livre" is for the Livre des procédures fiscales (2024-04-20)
+                relative_codes_article_ids.append(_id)
+
+    driver.quit()  # Exit now to avoid inconsistencies
+    return name, relative_codes_article_ids
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,16 +1,13 @@
-import bs4.element
-import pytest
 from bs4 import BeautifulSoup
 
-from src.utils import get_soup, get_non_abrogated_articles
+from src.utils import get_soup, get_code_non_abrogated_articles, get_article_data
 
-DATE: str = "2024-04-20"
-CODE_CIVIL_URL: str = "https://www.legifrance.gouv.fr/codes/texte_lc/LEGITEXT000006070721"
+CODE_CIVIL_ID: str = "LEGITEXT000006070721"
+CODE_CIVIL_ARTICLE_1_ID: str = "LEGIARTI000006419280"
+CODE_CIVIL_ARTICLE_2_ID: str = "LEGIARTI000006419281"
 
-
-@pytest.fixture()
-def soup():
-    return get_soup(CODE_CIVIL_URL + "/" + DATE)
+CODE_DEONTOLOGIE_ARCHITECTES_ID: str = "LEGITEXT000006074232"
+CODE_DEONTOLOGIE_ARCHITECTES_ARTICLE_1_ID: str = "LEGIARTI000006842411"
 
 
 def test_get_soup():
@@ -23,8 +20,57 @@ def test_get_soup():
     assert BeautifulSoup(text, "html.parser") == soup
 
 
-def test_get_non_abrogated_articles(soup):
-    non_abrogated_articles = get_non_abrogated_articles(soup)
+def test_get_code_non_abrogated_articles():
+    article_ids = get_code_non_abrogated_articles(CODE_CIVIL_ID)
 
-    assert isinstance(non_abrogated_articles, bs4.element.ResultSet)
-    assert len(non_abrogated_articles) == 2883
+    assert isinstance(article_ids, list)
+    assert len(article_ids) == 2883
+    assert isinstance(article_ids[0], str)
+    assert article_ids[0] == CODE_CIVIL_ARTICLE_1_ID
+
+    article_ids = get_code_non_abrogated_articles(CODE_DEONTOLOGIE_ARCHITECTES_ID)
+
+    assert isinstance(article_ids, list)
+    assert len(article_ids) == 48
+    assert isinstance(article_ids[0], str)
+    assert article_ids[0] == CODE_DEONTOLOGIE_ARCHITECTES_ARTICLE_1_ID
+
+
+def test_get_article_data():
+    # Article is quoted within other Codes
+    article_data = get_article_data(CODE_CIVIL_ARTICLE_1_ID)
+
+    assert isinstance(article_data, tuple)
+    assert len(article_data) == 2
+
+    assert isinstance(article_data[0], str)
+    assert article_data[0] == "Article 1"
+
+    assert isinstance(article_data[1], list)
+    assert len(article_data[1]) == 2
+    assert isinstance(article_data[1][0], str)
+    assert article_data[1] == ["LEGIARTI000031367546", "LEGIARTI000006450479"]
+
+    # Article is not quoted within other Codes
+    article_data = get_article_data(CODE_CIVIL_ARTICLE_2_ID)
+
+    assert isinstance(article_data, tuple)
+    assert len(article_data) == 2
+
+    assert isinstance(article_data[0], str)
+    assert article_data[0] == "Article 2"
+
+    assert isinstance(article_data[1], list)
+    assert len(article_data[1]) == 0
+
+    # Article is never quoted
+    article_data = get_article_data(CODE_DEONTOLOGIE_ARCHITECTES_ARTICLE_1_ID)
+
+    assert isinstance(article_data, tuple)
+    assert len(article_data) == 2
+
+    assert isinstance(article_data[0], str)
+    assert article_data[0] == "Article 1"
+
+    assert isinstance(article_data[1], list)
+    assert len(article_data[1]) == 0
